a73a634275ea69c76827fed2cf488d4458a27086
luccianosantana <lucianosantana@gmail.com>
2024-10-15 11:40:55 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.get("/", function(req, res) {
-    return res.send("Hello World");
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-app.listen(3000, function(){
-    console.log('Listening on port 3000');
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
