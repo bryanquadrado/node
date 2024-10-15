8b017c7ec1a7cf2b2396f12afbd9ce60a08a6315
luccianosantana <lucianosantana@gmail.com>
2024-10-15 11:43:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.status(200).send({
+    success: 'true',
+    message: 'Seja Bem-Vindo(a) ao mundo Docker!',
+    version: '1.0.0',
+  });
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(port);
+console.log('Aplicação executando na porta ', port);
